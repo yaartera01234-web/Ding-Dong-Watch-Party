c0b2c1d4399aba6185b91942dd7f3628ed292a53
Ding Dong <dingdong@users.noreply.github.com>
2026-09-20 13:48:05 +0000

    fix: fillMaxWidth import in status info

--- shared/src/commonMain/kotlin/app/room/ui/statinfo/RoomSectionStatusInfo.kt
+++ shared/src/commonMain/kotlin/app/room/ui/statinfo/RoomSectionStatusInfo.kt
@@ -10,6 +10,7 @@ import app.preferences.Preferences
 import app.preferences.set
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
